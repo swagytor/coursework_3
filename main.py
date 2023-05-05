15f43fefc09cd302ace24a5809a414baba5d85c4
Игнатенко Алексей <swagytor@yandex.ru>
2023-05-05 20:09:32 +0300

    update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ PATH_TO_JSON_DATA = os.path.join(PATH_TO_DATA, 'operations.json')
 def main():
     operations_data = utils.get_data_from_json(PATH_TO_JSON_DATA)
     sorted_data = utils.sort_data(operations_data)
+
     for data in sorted_data:
         utils.show(data)
 
